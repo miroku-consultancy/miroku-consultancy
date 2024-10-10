5eaf599950319a2b4d2d632fed171ba9499d4b66
avinashkr1 <avinashkr1@hotmail.com>
2024-10-10 09:11:57 +0530

    Added google map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,10 @@ const Contact = () => {
     if (!contactInfo) return <div>Loading...</div>; // Handle loading state
 
     return (
-        <section id="contact" style={{ ...styles.contactSection, backgroundImage: `url(${process.env.PUBLIC_URL}/${contactInfo.backgroundImage})` }}>
+        <section 
+            id="contact" 
+            style={{ ...styles.contactSection, backgroundImage: `url(${process.env.PUBLIC_URL}/${contactInfo.backgroundImage})` }}
+        >
             <h2>{contactInfo.heading}</h2>
             <p>{contactInfo.description}</p>
             <div style={styles.addressInfo}>
@@ -26,6 +29,16 @@ const Contact = () => {
             <div style={styles.contactInfo}>
                 <img src={`${process.env.PUBLIC_URL}/${contactInfo.email.icon}`} alt="Email Icon" style={styles.icon} />
                 <p>Email: <a href={`mailto:${contactInfo.email.address}`} style={styles.emailLink}>{contactInfo.email.address}</a></p>
+            </div>
+            {/* Google Map Embed */}
+            <div style={styles.mapContainer}>
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28013.976806652114!2d77.44156199999998!3d28.6373408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf1de56c6362d%3A0xcfd96615a8f6bf0a!2s428%2C%20Bhatia%20Rd%2C%20Kamla%20Quarter%2C%20Nehru%20Nagar%20III%2C%20Nehru%20Nagar%2C%20Ghaziabad%2C%20Uttar%20Pradesh%20201001!5e0!3m2!1sen!2sin!4v1728531440818!5m2!1sen!2sin" 
+                    style={styles.map}
+                    allowFullScreen 
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
             </div>
         </section>
     );
@@ -59,6 +72,18 @@ const styles = {
     },
     address: {
         fontSize: '1.2em',
+    },
+    mapContainer: {
+        marginTop: '20px',
+        width: '100%',
+        maxWidth: '600px', // Set a maximum width for the map
+        height: '300px', // Adjust height as necessary
+        margin: 'auto', // Center the map
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+        border: '0',
     },
 };
 
